fe3accef24b03067006feea40b1e99470a8280c1
Arthur Batista Furlan <arthurbfx2001@gmail.com>
2024-09-06 15:04:18 -0300

    v.0.1.1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,14 +63,6 @@
 				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
 					href="https://github.com/skeletonlabs/skeleton"
 					target="_blank"
 					rel="noreferrer"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -21,17 +21,12 @@
 		<div class="flex justify-center space-x-2">
 			<a
 				class="btn variant-filled-tertiary"
-				href="https://skeleton.dev/"
+				href="https://github.com/abfx2001"
 				target="_blank"
 				rel="noreferrer"
 			>
-				Launch Documentation
+				Conheça Meu Perfil
 			</a>
-		</div>
-		<div class="space-y-2">
-			<p>Try editing the following:</p>
-			<p><code class="code">/src/routes/+layout.svelte</code></p>
-			<p><code class="code">/src/routes/+page.svelte</code></p>
 		</div>
 	</div>
 </div>
